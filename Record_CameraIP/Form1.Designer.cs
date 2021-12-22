a06c0960ea992d178bb547d708512ac07e4238ea
phamvandan <phamvandan.cntt2@gmail.com>
2021-12-22 22:59:49 +0700

    ok

---
+++ Record_CameraIP/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Record_CameraIP
+{
+     partial class Form1
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.components = new System.ComponentModel.Container();
+               System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+               System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+               this.dataGridView1 = new System.Windows.Forms.DataGridView();
+               this.Camera_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.locationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.connect = new System.Windows.Forms.DataGridViewImageColumn();
+               this.Status = new System.Windows.Forms.DataGridViewImageColumn();
+               this.Check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+               this.cAMERABindingSource = new System.Windows.Forms.BindingSource(this.components);
+               this.cameraTrackingSystemDataSet = new Record_CameraIP.CameraTrackingSystemDataSet();
+               this.cAMERATableAdapter = new Record_CameraIP.CameraTrackingSystemDataSetTableAdapters.CAMERATableAdapter();
+               this.label2 = new System.Windows.Forms.Label();
+               this.groupBox1 = new System.Windows.Forms.GroupBox();
+               this.label6 = new System.Windows.Forms.Label();
+               this.panel2 = new System.Windows.Forms.Panel();
+               this.nUP_time_Save = new System.Windows.Forms.NumericUpDown();
+               this.label5 = new System.Windows.Forms.Label();
+               this.btnRecord_Single = new System.Windows.Forms.Button();
+               this.btnStop = new System.Windows.Forms.Button();
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.cAMERABindingSource)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.cameraTrackingSystemDataSet)).BeginInit();
+               this.groupBox1.SuspendLayout();
+               ((System.ComponentModel.ISupportInitialize)(this.nUP_time_Save)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // dataGridView1
+               // 
+               this.dataGridView1.AllowUserToAddRows = false;
+               this.dataGridView1.AutoGenerateColumns = false;
+               this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+               dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+               dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+               dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+               dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+               dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+               dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+               this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+               this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+               this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Camera_ID,
+            this.nameDataGridViewTextBoxColumn,
+            this.locationDataGridViewTextBoxColumn,
+            this.connect,
+            this.Status,
+            this.Check});
+               this.dataGridView1.DataSource = this.cAMERABindingSource;
+               this.dataGridView1.Location = new System.Drawing.Point(12, 36);
+               this.dataGridView1.Name = "dataGridView1";
+               this.dataGridView1.Size = new System.Drawing.Size(816, 440);
+               this.dataGridView1.TabIndex = 14;
+               // 
+               // Camera_ID
+               // 
+               this.Camera_ID.DataPropertyName = "Camera_ID";
+               this.Camera_ID.FillWeight = 68.01397F;
+               this.Camera_ID.HeaderText = "Mã Camera";
+               this.Camera_ID.Name = "Camera_ID";
+               this.Camera_ID.ReadOnly = true;
+               // 
+               // nameDataGridViewTextBoxColumn
+               // 
+               this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+               this.nameDataGridViewTextBoxColumn.FillWeight = 63.12585F;
+               this.nameDataGridViewTextBoxColumn.HeaderText = "Tên Camera";
+               this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+               // 
+               // locationDataGridViewTextBoxColumn
+               // 
+               this.locationDataGridViewTextBoxColumn.DataPropertyName = "Location";
+               this.locationDataGridViewTextBoxColumn.FillWeight = 224.0568F;
+               this.locationDataGridViewTextBoxColumn.HeaderText = "Vị Trí";
+               this.locationDataGridViewTextBoxColumn.Name = "locationDataGridViewTextBoxColumn";
+               // 
+               // connect
+               // 
+               this.connect.FillWeight = 68.58801F;
+               this.connect.HeaderText = "Kết Nối";
+               this.connect.Name = "connect";
+               // 
+               // Status
+               // 
+               this.Status.FillWeight = 59.88396F;
+               this.Status.HeaderText = "Lưu Trữ";
+               this.Status.Name = "Status";
+               // 
+               // Check
+               // 
+               this.Check.FalseValue = "";
+               this.Check.FillWeight = 56.80499F;
+               this.Check.HeaderText = "Lựa Chọn";
+               this.Check.Name = "Check";
+               // 
+               // cAMERABindingSource
+               // 
+               this.cAMERABindingSource.DataMember = "CAMERA";
+               this.cAMERABindingSource.DataSource = this.cameraTrackingSystemDataSet;
+               // 
+               // cameraTrackingSystemDataSet
+               // 
+               this.cameraTrackingSystemDataSet.DataSetName = "CameraTrackingSystemDataSet";
+               this.cameraTrackingSystemDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+               // 
+               // cAMERATableAdapter
+               // 
+               this.cAMERATableAdapter.ClearBeforeFill = true;
+               // 
+               // label2
+               // 
+               this.label2.AutoSize = true;
+               this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label2.Location = new System.Drawing.Point(368, 12);
+               this.label2.Name = "label2";
+               this.label2.Size = new System.Drawing.Size(125, 16);
+               this.label2.TabIndex = 15;
+               this.label2.Text = "Danh Sách Camera";
+               // 
+               // groupBox1
+               // 
+               this.groupBox1.Controls.Add(this.label6);
+               this.groupBox1.Controls.Add(this.panel2);
+               this.groupBox1.Controls.Add(this.nUP_time_Save);
+               this.groupBox1.Controls.Add(this.label5);
+               this.groupBox1.Controls.Add(this.btnRecord_Single);
+               this.groupBox1.Controls.Add(this.btnStop);
+               this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.groupBox1.Location = new System.Drawing.Point(12, 499);
+               this.groupBox1.Name = "groupBox1";
+               this.groupBox1.Size = new System.Drawing.Size(816, 77);
+               this.groupBox1.TabIndex = 16;
+               this.groupBox1.TabStop = false;
+               this.groupBox1.Text = "Điều Khiển";
+               // 
+               // label6
+               // 
+               this.label6.AutoSize = true;
+               this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label6.Location = new System.Drawing.Point(325, 30);
+               this.label6.Name = "label6";
+               this.label6.Size = new System.Drawing.Size(31, 15);
+               this.label6.TabIndex = 14;
+               this.label6.Text = "phút";
+               // 
+               // panel2
+               // 
+               this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+               this.panel2.Location = new System.Drawing.Point(276, 47);
+               this.panel2.Name = "panel2";
+               this.panel2.Size = new System.Drawing.Size(24, 1);
+               this.panel2.TabIndex = 0;
+               // 
+               // nUP_time_Save
+               // 
+               this.nUP_time_Save.BorderStyle = System.Windows.Forms.BorderStyle.None;
+               this.nUP_time_Save.Location = new System.Drawing.Point(276, 32);
+               this.nUP_time_Save.Name = "nUP_time_Save";
+               this.nUP_time_Save.Size = new System.Drawing.Size(43, 17);
+               this.nUP_time_Save.TabIndex = 0;
+               // 
+               // label5
+               // 
+               this.label5.AutoSize = true;
+               this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label5.Location = new System.Drawing.Point(170, 30);
+               this.label5.Name = "label5";
+               this.label5.Size = new System.Drawing.Size(100, 15);
+               this.label5.TabIndex = 13;
+               this.label5.Text = "Thời lượng video:";
+               // 
+               // btnRecord_Single
+               // 
+               this.btnRecord_Single.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.btnRecord_Single.Location = new System.Drawing.Point(452, 26);
+               this.btnRecord_Single.Name = "btnRecord_Single";
+               this.btnRecord_Single.Size = new System.Drawing.Size(87, 23);
+               this.btnRecord_Single.TabIndex = 9;
+               this.btnRecord_Single.Text = "Bắt Đầu Lưu";
+               this.btnRecord_Single.UseVisualStyleBackColor = true;
+               this.btnRecord_Single.Click += new System.EventHandler(this.btnRecord_Single_Click);
+               // 
+               // btnStop
+               // 
+               this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.btnStop.Location = new System.Drawing.Point(599, 26);
+               this.btnStop.Name = "btnStop";
+               this.btnStop.Size = new System.Drawing.Size(87, 23);
+               this.btnStop.TabIndex = 8;
+               this.btnStop.Text = "Dừng Lưu";
+               this.btnStop.UseVisualStyleBackColor = true;
+               this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+               // 
+               // Form1
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(835, 589);
+               this.Controls.Add(this.groupBox1);
+               this.Controls.Add(this.label2);
+               this.Controls.Add(this.dataGridView1);
+               this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+               this.Name = "Form1";
+               this.Text = "Lưu Video";
+               this.Load += new System.EventHandler(this.Form1_Load);
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.cAMERABindingSource)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.cameraTrackingSystemDataSet)).EndInit();
+               this.groupBox1.ResumeLayout(false);
+               this.groupBox1.PerformLayout();
+               ((System.ComponentModel.ISupportInitialize)(this.nUP_time_Save)).EndInit();
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
+          }
+
+          #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CameraTrackingSystemDataSet cameraTrackingSystemDataSet;
+        private System.Windows.Forms.BindingSource cAMERABindingSource;
+        private CameraTrackingSystemDataSetTableAdapters.CAMERATableAdapter cAMERATableAdapter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown nUP_time_Save;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRecord_Single;
+        private System.Windows.Forms.Button btnStop;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Camera_ID;
+          private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+          private System.Windows.Forms.DataGridViewTextBoxColumn locationDataGridViewTextBoxColumn;
+          private System.Windows.Forms.DataGridViewImageColumn connect;
+          private System.Windows.Forms.DataGridViewImageColumn Status;
+          private System.Windows.Forms.DataGridViewCheckBoxColumn Check;
+     }
+}
+
---
+++ Record_CameraIP/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using RestSharp;
+using Method = RestSharp.Method;
+using System.Threading;
+
+namespace Record_CameraIP
+{
+     public partial class Form1 : Form
+     {
+          private int save_Time;
+          private string save_Link;
+
+          string conStr = "data source=192.168.10.65;initial catalog=CameraTrackingSystem;user id=sa;password=123456;MultipleActiveResultSets=True;App=EntityFramework";
+
+          Image img_tick = Image.FromFile(@"C:\Users\User\Downloads\iloveimg-resized\tick.png");
+          Image img_close = Image.FromFile(@"C:\Users\User\Downloads\iloveimg-resized\close.png");
+
+          int run_first = 0;
+
+          public Form1()
+          {
+               InitializeComponent();
+               SqlConnection sqlCon = new SqlConnection(conStr);
+               sqlCon.Open();
+
+               string qry_numOf_Cam = "select Camera_ID,Name,Location from CAMERA";
+               SqlCommand cmd_numOf_Cam = new SqlCommand(qry_numOf_Cam, sqlCon);
+
+               SqlDataAdapter sda_numOf_Cam = new SqlDataAdapter(cmd_numOf_Cam);
+               DataTable dt_Cam = new DataTable();
+               sda_numOf_Cam.Fill(dt_Cam);
+
+               dataGridView1.DataSource = dt_Cam;
+
+               
+
+
+
+
+               sqlCon.Close();
+
+               dataGridView1.RowTemplate.Height = 40;
+
+               //for (int i = 0; i < dataGridView1.Rows.Count; i++)
+               //{
+               //     dataGridView1.Rows[i].Cells[5].Value = false;
+               //}
+
+              
+               run_first = 0;
+
+               
+
+              
+          }
+
+          private void Form1_Load(object sender, EventArgs e)
+          {
+               // TODO: This line of code loads data into the 'cameraTrackingSystemDataSet.CAMERA' table. You can move, or remove it, as needed.
+               this.cAMERATableAdapter.Fill(this.cameraTrackingSystemDataSet.CAMERA);
+
+               ///////////////////////////////////////////////////////////////////////////////////// Lấy trạng thái đang lưu hay không của camera
+               SqlConnection sqlCon = new SqlConnection(conStr);
+               sqlCon.Open();
+
+               
+
+               if (run_first == 0)
+               {
+                    
+                    string qry_RTSP_url = "select RTSP_URL from CAMERA";
+
+                    SqlCommand cmd_RTSP_url = new SqlCommand(qry_RTSP_url, sqlCon);
+
+                    SqlDataAdapter sda_RTSP_url = new SqlDataAdapter(cmd_RTSP_url);
+                    DataTable dt_RTSP_url = new DataTable();
+                    sda_RTSP_url.Fill(dt_RTSP_url);
+
+                    int k = 0;
+
+                    foreach (DataRow row in dt_RTSP_url.Rows)
+                    {
+                         string url = row["RTSP_URL"].ToString();
+                         var client = new RestClient("http://localhost:8000/get_connection_status");
+                         var request = new RestRequest(Method.GET);
+                         request.AddHeader("postman-token", "8cb64f2e-b33f-4f08-ecaf-cafd9a3e4260");
+                         request.AddHeader("cache-control", "no-cache");
+                         request.AddOrUpdateHeader("connection_path", url);
+                         IRestResponse response = client.Execute(request);
+                         string status = response.Content.ToString();
+
+                         if (status == "running")
+                         {
+                              dataGridView1.Rows[k].Cells[4].Value = img_tick;
+                         }
+                         else
+                         {
+                              dataGridView1.Rows[k].Cells[4].Value = img_close;
+                         }
+
+                         k++;
+                    }
+
+                    
+               }
+
+
+               var client_1 = new RestClient("http://localhost:8000/check_camera_status");
+               var request_1 = new RestRequest(Method.GET);
+               request_1.AddHeader("postman-token", "92a0bba7-2cb0-ccee-caff-61e1c762fd7b");
+               request_1.AddHeader("cache-control", "no-cache");
+
+               for (int i = 0; i < dataGridView1.Rows.Count; i++)
+               {
+                    cam_ID = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    string qry_RTSP = "select RTSP_URL from CAMERA where Camera_ID = " + cam_ID;
+                    SqlCommand cmd_RTSP = new SqlCommand(qry_RTSP, sqlCon);
+
+                    link_RTSP = cmd_RTSP.ExecuteScalar().ToString();
+                    request_1.AddOrUpdateHeader("connection_path", link_RTSP);
+
+                    IRestResponse response = client_1.Execute(request_1);
+
+                    string status = response.Content;
+
+                    if (status == "Online")
+                    {
+                         dataGridView1.Rows[i].Cells[3].Value = img_tick;
+                         dataGridView1.Rows[i].Cells[5].ReadOnly = false;
+                    }
+                    else
+                    {
+                         dataGridView1.Rows[i].Cells[3].Value = img_close;
+                         dataGridView1.Rows[i].Cells[5].ReadOnly = true;
+                    }
+               }
+
+
+
+               sqlCon.Close();
+               for (int i = 0; i < dataGridView1.Rows.Count; i++)
+               {
+                    dataGridView1.Rows[i].Cells[5].Value = false;
+               }
+
+               run_first = 1;
+          }
+
+          
+
+         
+
+          private void btnRecord_Single_Click(object sender, EventArgs e)
+          {
+               nUP_time_Save.Enabled = false;
+
+               SqlConnection sqlCon = new SqlConnection(conStr);
+               sqlCon.Open();
+
+               var client = new RestClient("http://localhost:8000/do_record");
+
+               var request = new RestRequest(Method.POST);
+               request.AddHeader("postman-token", "5a83ff14-ec3c-ef62-6abb-382f3e64625e");
+               request.AddHeader("cache-control", "no-cache");
+               request.AddHeader("movement_detected_persistence", "11");
+               request.AddHeader("min_diff_for_movement", "1");
+               request.AddHeader("frames_to_persist", "10");
+               request.AddHeader("save_fps", "15");
+               request.AddHeader("interval", nUP_time_Save.Value.ToString());
+               //request.AddHeader("connection_path", link_RTSP);
+               //request.AddHeader("camera_id", cam_ID);
+               //IRestResponse response = client.Execute(request);
+
+
+               for (int i = 0; i < dataGridView1.Rows.Count; i++)
+               {
+                    if(dataGridView1.Rows[i].Cells[5].Value != "")
+                    {
+                         bool isCellChecked = (bool)dataGridView1.Rows[i].Cells[5].Value;
+                         if (isCellChecked == true)
+                         {
+                              dataGridView1.Rows[i].Cells[4].Value = img_tick;
+                              cam_ID = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                              string qry_RTSP = "select RTSP_URL from CAMERA where Camera_ID = " + cam_ID;
+                              SqlCommand cmd_RTSP = new SqlCommand(qry_RTSP, sqlCon);
+
+                              link_RTSP = cmd_RTSP.ExecuteScalar().ToString();
+                              request.AddOrUpdateHeader("connection_path", link_RTSP);
+                              request.AddHeader("camera_id", cam_ID);
+                              IRestResponse response = client.Execute(request);
+
+                              string status = response.Content;
+                              
+                              if(status == "success")
+                              {
+                                   dataGridView1.Rows[i].Cells[4].Value = img_tick;
+                              }
+                              else
+                              {
+                                   MessageBox.Show("Camera số " + cam_ID + " đang được lưu");
+                              }
+
+                              
+                         }
+                         else if (dataGridView1.Rows[i].Cells[4].Value != img_tick)
+                         {
+                              dataGridView1.Rows[i].Cells[4].Value = img_close;
+                         }
+                    }
+                    
+
+               }
+
+               sqlCon.Close();
+
+               Form1_Load(sender, e);
+          }
+
+          string cam_ID;
+          string link_RTSP;
+
+          List<string> id_cam;
+
+          private void btnStop_Click(object sender, EventArgs e)
+          {
+               nUP_time_Save.Enabled = true;
+
+               SqlConnection sqlCon = new SqlConnection(conStr);
+               sqlCon.Open();
+
+               var client = new RestClient("http://localhost:8000/remove_connection");
+
+               var request = new RestRequest(Method.GET);
+               request.AddHeader("postman-token", "5a83ff14-ec3c-ef62-6abb-382f3e64625e");
+               request.AddHeader("cache-control", "no-cache");
+               
+               //request.AddHeader("connection_path", link_RTSP);
+               //request.AddHeader("camera_id", cam_ID);
+               //IRestResponse response = client.Execute(request);
+
+
+               for (int i = 0; i < dataGridView1.Rows.Count; i++)
+               {
+                    if (dataGridView1.Rows[i].Cells[5].Value != "")
+                    {
+                         bool isCellChecked = (bool)dataGridView1.Rows[i].Cells[5].Value;
+                         if (isCellChecked == true)
+                         {
+                              cam_ID = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                              string qry_RTSP = "select RTSP_URL from CAMERA where Camera_ID = " + cam_ID;
+                              SqlCommand cmd_RTSP = new SqlCommand(qry_RTSP, sqlCon);
+
+                              link_RTSP = cmd_RTSP.ExecuteScalar().ToString();
+
+                              request.AddOrUpdateHeader("connection_path", link_RTSP);
+                              
+                              IRestResponse response = client.Execute(request);
+
+                              string status = response.Content;
+
+                              if(status == "removed")
+                              {
+                                   dataGridView1.Rows[i].Cells[4].Value = img_close;
+                              }
+                              else
+                              {
+                                   MessageBox.Show("Chưa thể tắt trạng thái lưu");
+                              }
+                              
+                         }
+                         else if (dataGridView1.Rows[i].Cells[4].Value != img_tick)
+                         {
+                              dataGridView1.Rows[i].Cells[4].Value = img_close;
+                         }
+                    }
+
+
+               }
+
+               sqlCon.Close();
+
+               Form1_Load(sender, e);
+          }
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
